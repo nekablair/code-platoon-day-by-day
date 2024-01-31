62c02e0b570e7a6acd1192ec994b5de1cf9b329d
Neka Blair <ourblairfamily@gmail.com>
2024-01-31 09:16:53 -0600

    added more notes about intro

--- scratchpad.js
+++ scratchpad.js
@@ -1,3 +1,16 @@
+// // features of every programming lanugage:
+// -syntax 
+// -logical operators (if, and, else)
+// -primitive data types -strings numbers booleans
+// -complex data types-arrays, objects, dictionaries
+// -weakly typed languages are also called "dynamic" languages
+// --if able to change type of variable from one thing to another
+// --JS and Python are both weakly typed languages
+//-how do you run the code? one to compile or another to compiled source code?
+// -strongly typed languages need compiler to take human readable code and
+// turn it into a machine/computer(assembly) language to run it
+
+
 //why js matters and internet
 
 //node.js
